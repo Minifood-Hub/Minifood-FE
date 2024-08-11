f0cc89e142ced7e9c9248d12374f1213d63f4ab3
김현중 <kjyse2@naver.com>
2024-08-11 22:24:21 +0900

    ✨[관리자]: 야채 물품 가격 직접 변경 api 연결

--- src/app/components/admin/products/ProductsVegetable.tsx
+++ src/app/components/admin/products/ProductsVegetable.tsx
@@ -1,19 +1,30 @@
+// 야채 물품 가격 직접 변경
+
 import { useState } from 'react';
 import Input from '../../common/Input';
 import Button from '../../common/Button';
-import { ALERT_TEXT, BTN_TEXT, INPUT_TEXT } from '@/app/constants/admin';
+import {
+  ALERT_TEXT,
+  BTN_TEXT,
+  INPUT_TEXT,
+  TABLE_TEXT,
+} from '@/app/constants/admin';
 import { callPatch } from '@/app/utils/callApi';
 
 export default function ProductsVegetable() {
   const [productId, setProductId] = useState('');
+  const [price, setPrice] = useState('');
 
   const handlePatch = async () => {
-    if (!productId) {
+    if (!productId || !price) {
       alert(ALERT_TEXT[0]);
       return;
     }
     try {
-      // await callPatch();
+      await callPatch(
+        `/api/admin/products/${productId}/vegetable`,
+        `price=${price}`,
+      );
     } catch (error) {
       console.error(error);
     }
@@ -23,12 +34,22 @@ export default function ProductsVegetable() {
       <div className="flex gap-4 items-center">
         <p className="whitespace-nowrap">{INPUT_TEXT[9]}</p>
         <Input
-          name="inputComment"
           className="admin-input"
           type="default"
           onChange={(e) => setProductId(e.target.value)}
           textValue={productId}
           placeholder={INPUT_TEXT[9]}
+        />
+      </div>
+
+      <div className="flex gap-4 items-center">
+        <p className="whitespace-nowrap">{TABLE_TEXT[5]}</p>
+        <Input
+          className="admin-input"
+          type="default"
+          onChange={(e) => setPrice(e.target.value)}
+          textValue={price}
+          placeholder={TABLE_TEXT[5]}
         />
       </div>
 
