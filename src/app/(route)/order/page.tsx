1b3d416906108decbb2184654c77557998ac87ab
김현중 <kjyse2@naver.com>
2024-08-14 18:57:23 +0900

    ✨[상품주문]: 기타 UI 세팅 진행

--- src/app/(route)/order/page.tsx
+++ src/app/(route)/order/page.tsx
@@ -4,14 +4,12 @@ import { SIDENAV_TEXT } from '@/app/constants/common';
 
 export default function Order() {
   return (
-    <section>
-      <div className="bg-white w-full h-screen flex relative">
-        {/* 네비게이션 바 */}
-        <SideNavBar selected={SIDENAV_TEXT[1]} />
+    <section className="bg-gray-0 min-w-full h-screen flex relative">
+      {/* 네비게이션 바 */}
+      <SideNavBar selected={SIDENAV_TEXT[1]} />
 
-        {/* 메인 컨테이너 */}
-        <OrderContainer />
-      </div>
+      {/* 메인 컨테이너 */}
+      <OrderContainer />
     </section>
   );
 }
