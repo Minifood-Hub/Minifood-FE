538fb446834e1b434adcf24c73269117fd23e6f1
Minkyu0424 <yng1404@gmail.com>
2024-08-17 13:44:00 +0900

    :sparkles: [공통] : 날짜 포맷함수 요일반환

--- src/app/constants/common.ts
+++ src/app/constants/common.ts
@@ -46,3 +46,15 @@ export const SELECTED_SIDENAV_ICONS = [
 ];
 
 export const SIDENAV_PATH = ['/order', '/past-order', '/quotation', '/'];
+
+export const DAYS_OF_WEEEK = [
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+  '일요일',
+];
+
+export const DAYS_OF_WEEK_SHORTEN = ['월', '화', '수', '목', '금', '토', '일'];
--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -1,7 +1,13 @@
-export const formatDate = (isoDate: any): string => {
+import { DAYS_OF_WEEEK } from '../constants/common';
+
+export const formatDate = (isoDate: any): FormatDateResult => {
   const date = new Date(isoDate);
+  const weekday = date.getDay();
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return {
+    date: `${year}-${month}-${day}`,
+    day: DAYS_OF_WEEEK[weekday],
+  };
 };
