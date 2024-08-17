fa23f94da2324e419be264c938415b3f97114541
Minkyu0424 <yng1404@gmail.com>
2024-08-17 21:45:48 +0900

    :recycle: [공통] : h-full 속성 삭제

--- src/app/components/common/layout/SideNavBar.tsx
+++ src/app/components/common/layout/SideNavBar.tsx
@@ -17,7 +17,7 @@ export default function SideNavBar({ selected }: SideNavBarProps) {
     return selectedMenu === selected;
   };
   return (
-    <div className="bg-white w-64 h-full flex flex-col items-center rounded-[20px] pt-9 shadow-xl">
+    <div className="bg-white w-64 flex flex-col items-center rounded-[20px] pt-9 shadow-xl">
       <Link href="/">
         <Image src="/Images/JMF2.png" alt="로고" width={100} height={75} />
       </Link>
