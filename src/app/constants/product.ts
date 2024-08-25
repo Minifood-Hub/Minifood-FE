e975399f25a4749d1b967a04d0832d810169e753
Minkyu0424 <yng1404@gmail.com>
2024-08-26 02:13:24 +0900

    :recycle:[공통] : 코드 포매팅

--- src/app/constants/product.ts
+++ src/app/constants/product.ts
@@ -1,3 +1,9 @@
 export const PRODUCT_HEADER = ['상품조회', '검색어를 입력해주세요'];
 
-export const PRODUCT_CATEGORY:ProductTypes[] = ['전체', '야채', '냉동', '냉장', '공산제품'];
+export const PRODUCT_CATEGORY: ProductTypes[] = [
+  '전체',
+  '야채',
+  '냉동',
+  '냉장',
+  '공산제품',
+];
