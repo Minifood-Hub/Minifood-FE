c217a845223de7b52688fec32b0cc1d7d11855dd
Minkyu0424 <yng1404@gmail.com>
2024-08-28 21:04:06 +0900

    :recycle: [메인] : 커스텀 상품 조회 타입생성, 카드 텍스트 수정

--- src/app/components/main/recommend/Card.tsx
+++ src/app/components/main/recommend/Card.tsx
@@ -1,30 +1,23 @@
 'use client';
 
-import { formatPrice } from '@/app/utils/formatPrice';
 import Image from 'next/image';
 import Button from '../../common/Button';
 
 interface RecommendCardProps {
-  itemName: string;
-  amount: number;
-  category: string;
-  price: number;
-  orderItem: () => void;
+  customProducts: CustomProductType;
 }
 
-function RecommendCard({
-  itemName,
-  amount,
-  category,
-  price,
-  orderItem,
-}: RecommendCardProps) {
+function RecommendCard({ customProducts }: RecommendCardProps) {
+  const orderItem = () => {
+    console.log('구매 이동 안내');
+  };
+
   return (
     <div className="flex flex-col w-[244px] h-[431px] rounded-[4px]">
       <div className="w-[244px] h-[320px] relative rounded">
         <Image
           src="/Images/tomato.png"
-          alt="카드 이미지"
+          alt="card_image"
           layout="fill"
           objectFit="cover"
           className="rounded"
@@ -32,13 +25,13 @@ function RecommendCard({
         <div className="absolute inset-0 bg-gray-300 opacity-0 hover:opacity-70 transition-opacity duration-300 rounded" />
       </div>
       <Button
-        buttonText="담기"
+        buttonText="바로담기"
         type="recommendButton"
         onClickHandler={orderItem}
       />
-      <div className="flex flex-col gap-y-3 mt-3 text-base font-normal">
-        <div>{itemName}</div>
-        <div>{formatPrice(price)}원</div>
+      <div className="flex mt-3 text-base font-normal gap-x-1">
+        <div>{customProducts.name}</div>
+        <div>{customProducts.unit}</div>
       </div>
     </div>
   );
