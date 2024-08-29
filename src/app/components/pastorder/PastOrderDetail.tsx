f0aab72cd59c9af14da41d7b83e55c2c5b6dc1df
Minkyu0424 <yng1404@gmail.com>
2024-08-29 18:30:33 +0900

    :recycle: [즐겨찾기] : 즐겨찾기 카테고리명 분류

--- src/app/components/pastorder/PastOrderDetail.tsx
+++ src/app/components/pastorder/PastOrderDetail.tsx
@@ -1,3 +1,4 @@
+import { categoryMapping } from '@/app/constants/order';
 import { PASTORDER_DETAIL } from '@/app/constants/pastorder';
 import { callGet } from '@/app/utils/callApi';
 import { useEffect, useState } from 'react';
@@ -31,7 +32,9 @@ const PastOrderDetail = ({ pastorderId }: PastOrderDetailProps) => {
           className="w-full flex h-[54px] items-center text-base font-normal"
           key={pastOrder.id}
         >
-          <div className="w-[9.1%] text-center">{pastOrder.category}</div>
+          <div className="w-[9.1%] text-center">
+            {categoryMapping[pastOrder.category]}
+          </div>
           <div className="w-[18.6%] pl-[14px]">{i + 1}</div>
           <div className="w-[49.1%] pl-[14px]">{pastOrder.name}</div>
           <div className="w-[11.4%] text-center">{pastOrder.unit}</div>
