87318020f6fa3d9cd9c3c4976f24b44e5ddbe2a7
Minkyu0424 <yng1404@gmail.com>
2024-09-14 17:39:25 +0900

    :sparklse:[메인] : 메인 캐러셀 이미지 미반영

--- src/app/components/main/carousel/MainCard.tsx
+++ src/app/components/main/carousel/MainCard.tsx
@@ -1,10 +1,14 @@
 import { CUSTOM_MAINCARD_TEXT } from '@/app/constants/custom';
+import Image from 'next/image';
 
 function MainCard() {
   return (
-    <div className="flex flex-col w-full h-[360px] rounded-[4px] bg-gradient-to-t from-[#fee3c7] to-[#f7d4b4]">
-      <p>{CUSTOM_MAINCARD_TEXT[0].firstline}</p>
-      <p>{CUSTOM_MAINCARD_TEXT[1].secondlinre}</p>
+    <div className="flex w-full h-[360px] bg-gradient-to-t from-[#fee3c7] to-[#f7d4b4] items-center justify-center gap-x-[112px]">
+      <div className="flex">
+        <p>{CUSTOM_MAINCARD_TEXT[0].firstline}</p>
+        <p>{CUSTOM_MAINCARD_TEXT[1].secondlinre}</p>
+      </div>
+      <Image src="/Images/banner1.png" alt="banner" width={427} height={360} />
     </div>
   );
 }
