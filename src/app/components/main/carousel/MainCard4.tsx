a7b804b76da2b7fe32db6e31b3cb81c2ca972115
Minkyu0424 <yng1404@gmail.com>
2024-09-15 13:47:52 +0900

    :sparkles:[메인]  : 배너 사진 변경

--- src/app/components/main/carousel/MainCard4.tsx
+++ src/app/components/main/carousel/MainCard4.tsx
@@ -3,22 +3,20 @@ import Image from 'next/image';
 
 function MainCard4() {
   return (
-    <div className="flex w-full h-[360px] bg-gradient-to-r from-[#d5b185] to-[#cd9654] items-center justify-center gap-x-[112px]">
+    <div className="flex w-full h-[360px] bg-gradient-to-r from-[#d5b185] to-[#cd9654] items-center justify-center gap-x-[112px] z-10">
       <div className="flex flex-col text-2xl text-white">
         <p className="font-normal leading-9">
           {CUSTOM_MAINCARD_TEXT[0].firstline}
         </p>
         <p className="font-bold">{CUSTOM_MAINCARD_TEXT[1].secondlinre}</p>
       </div>
-      <div className="relative w">
-        <Image
-          src="/Images/banner4.png"
-          alt="banner"
-          layout="fill"
-          objectFit="contain"
-          className="rounded-full"
-        />
-      </div>
+      <Image
+        src="/Images/banner5.png"
+        alt="banner"
+        className="rounded-full"
+        width={400}
+        height={320}
+      />
     </div>
   );
 }
