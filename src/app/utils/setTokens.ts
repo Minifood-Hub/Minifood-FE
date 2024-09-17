e2f887f9b3fef73ef14bdfa84872bfd44a87763e
kimgorok <kjyse2@naver.com>
2024-09-17 13:00:51 +0900

    ✨[공통]: 세션 타임아웃 수정

--- src/app/utils/setTokens.ts
+++ src/app/utils/setTokens.ts
@@ -10,7 +10,7 @@ const setSessionTimeout = (duration: number) => {
   sessionTimeoutId = window.setTimeout(() => {
     alert('세션이 만료되었습니다. 다시 로그인해 주세요.');
     window.location.href = '/sign-in';
-  }, duration - 5000); // 만료 5초 전에 알림
+  }, duration - 3000); // 만료 3초 전에 알림
 };
 
 export const setTokens = (
@@ -19,18 +19,19 @@ export const setTokens = (
   isRefresh: boolean = false,
 ) => {
   try {
-    const accessTokenExpires = new Date(Date.now() + 1000 * 60 * 30); // 30분
+    const accessTokenExpires = isRefresh
+      ? new Date(Date.now() + 1000 * 60 * 120) // 연장된 토큰 시간 (120분)
+      : new Date(Date.now() + 1000 * 60 * 30); // 기본 토큰 시간 (30분)
+
     const accessTokenExpiresUTC = accessTokenExpires.toUTCString();
 
     const refreshTokenExpires = new Date(Date.now() + 1000 * 60 * 120); // 120분
     const refreshTokenExpiresUTC = refreshTokenExpires.toUTCString();
 
-    document.cookie = `accessToken=${accessToken}; expires=${accessTokenExpiresUTC}; path=/; secure; HttpOnly; SameSite=Strict`;
-    document.cookie = `refreshToken=${refreshToken}; expires=${refreshTokenExpiresUTC}; path=/; secure; HttpOnly; SameSite=Strict`;
+    document.cookie = `accessToken=${accessToken}; expires=${accessTokenExpiresUTC}; path=/; secure;`;
+    document.cookie = `refreshToken=${refreshToken}; expires=${refreshTokenExpiresUTC}; path=/; secure;`;
 
-    const timeoutDuration = isRefresh
-      ? 1000 * 60 * 119 + 1000 * 58
-      : 1000 * 60 * 29 + 1000 * 58;
+    const timeoutDuration = accessTokenExpires.getTime() - Date.now();
     setSessionTimeout(timeoutDuration);
   } catch (error) {
     console.error('토큰 설정 중 오류 발생:', error);
