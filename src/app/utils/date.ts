5b017f7d768e6034084eff9e9d8313cdbd7f9504
kimgorok <kjyse2@naver.com>
2024-09-19 15:12:50 +0900

    ✨[관리자]: 공지사항 생성일/수정일 표시 수정

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -13,3 +13,14 @@ export const getDay = (isoDate: any) => {
   const weekday = date.getDay();
   return DAYS_OF_WEEEK[weekday];
 };
+
+export const formatDateTime = (dateTimeString: string) => {
+  const date = new Date(dateTimeString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${year}년${month}월${day}일${hours}시${minutes}분`;
+};
