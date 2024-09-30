1e87f9c5952c91214e881d3bed630c5a6fb359be
kimgorok <kjyse2@naver.com>
2024-09-30 21:31:02 +0900

    ✨[헤더,사이드바]: 세션 연장 함수 적용

--- src/app/components/common/layout/Header.tsx
+++ src/app/components/common/layout/Header.tsx
@@ -8,10 +8,10 @@ import {
   HEADER_TEXT,
 } from '@/app/constants/common';
 import { useUser } from '@/app/hooks/useUser';
-import { setTokens } from '@/app/utils/setTokens';
 import Image from 'next/image';
 import Link from 'next/link';
 import ProfileDropDown from '../ProfileDropDown';
+import { handleExtendSession } from '@/app/utils/extendSession';
 
 function Header() {
   const { user } = useUser();
@@ -20,40 +20,6 @@ function Header() {
     document.cookie = `accessToken=; expires=0; path=/;`;
     document.cookie = `refreshToken=; expires=0; path=/;`;
     window.location.href = '/';
-  };
-
-  const handleExtendSession = async () => {
-    // 쿠키에서 리프레시 토큰을 가져옴
-    const cookies = document.cookie.split(';');
-    const refreshToken = cookies
-      .find((cookie) => cookie.trim().startsWith('refreshToken='))
-      ?.split('=')[1];
-
-    try {
-      const response = await fetch('/api/account/refresh', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ refreshToken }),
-      });
-
-      if (response.ok) {
-        const responseData = await response.json();
-        const newAccessToken = responseData.result.access_token;
-        const newAccessRefreshToken = responseData.result.refresh_token;
-
-        // 새로운 액세스 토큰을 쿠키에 저장
-        if (refreshToken) {
-          setTokens(newAccessToken, newAccessRefreshToken, true);
-        }
-
-        alert('세션이 연장되었습니다.');
-      }
-    } catch (error) {
-      console.error('세션 연장 에러:', error);
-      alert('세션 연장 중 오류가 발생했습니다.');
-    }
   };
 
   const headerPath = user && user?.category ? HEADER_PATH : HEADER_PATH_GUEST;
--- src/app/components/common/layout/SideNavBar.tsx
+++ src/app/components/common/layout/SideNavBar.tsx
@@ -6,10 +6,10 @@ import {
   SIDENAV_PATH,
   SIDENAV_TEXT,
 } from '@/app/constants/common';
-import { setTokens } from '@/app/utils/setTokens';
 import Image from 'next/image';
 import Link from 'next/link';
 import Icons from '../Icons';
+import { handleExtendSession } from '@/app/utils/extendSession';
 
 interface SideNavBarProps {
   selected: string;
@@ -20,39 +20,6 @@ export default function SideNavBar({ selected }: SideNavBarProps) {
     return selectedMenu === selected;
   };
 
-  const handleExtendSession = async () => {
-    // 쿠키에서 리프레시 토큰을 가져옴
-    const cookies = document.cookie.split(';');
-    const refreshToken = cookies
-      .find((cookie) => cookie.trim().startsWith('refreshToken='))
-      ?.split('=')[1];
-
-    try {
-      const response = await fetch('/api/account/refresh', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ refreshToken }),
-      });
-
-      if (response.ok) {
-        const responseData = await response.json();
-        const newAccessToken = responseData.result.access_token;
-        const newAccessRefreshToken = responseData.result.refresh_token;
-
-        // 새로운 액세스 토큰을 쿠키에 저장
-        if (refreshToken) {
-          setTokens(newAccessToken, newAccessRefreshToken, true);
-        }
-
-        alert('세션이 연장되었습니다.');
-      }
-    } catch (error) {
-      console.error('세션 연장 에러:', error);
-      alert('세션 연장 중 오류가 발생했습니다.');
-    }
-  };
   return (
     <div className="bg-white w-64 h-full flex flex-col items-center rounded-[20px] pt-9 shadow-xl relative">
       <Link href="/">
