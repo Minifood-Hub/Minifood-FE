2283db15125011257379e57d9f9460362bd5bcb4
Minkyu0424 <yng1404@gmail.com>
2024-10-03 23:10:40 +0900

    :ambulance:[hotfix] : 핫픽스-상품조회 api 삭제

--- src/app/api/product/get/route.ts
+++
@@ -1,7 +0,0 @@
-import { getProducts } from '@/app/service/getRequest';
-import { NextResponse } from 'next/server';
-
-export async function GET(req: Request): Promise<NextResponse> {
-  const data = await getProducts(req);
-  return NextResponse.json(data);
-}
